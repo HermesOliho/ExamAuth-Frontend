fa4885e6f4bd259c48b314b2ce9730f5457dfc0a
Hermès Oliho <97853199+HermesOliho@users.noreply.github.com>
2024-12-28 10:53:44 +0200

    Update versions.ts
    Mise à jour du délai d'essai

--- src/functions/versions.ts
+++ src/functions/versions.ts
@@ -4,7 +4,7 @@ export const canUseApp = () => {
     const month = today.getMonth();
     const daysDate = today.getDate();
     const condition1 =
-        year === 2024 && (month < 11 || (month === 11 && daysDate < 28));
+        year === 2024 && (month < 11 || (month === 11 && daysDate <= 30));
     const condition2 = year < 2024;
     console.log(daysDate, month, condition1);
     return condition1 || condition2;
